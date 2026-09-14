74a81b3126f205e5f3b607d00e6c7973dc69ecb7
rtesteqwer <rtesteqwer@gmail.com>
2026-09-14 09:04:00 -0300

    Configurar módulo Android online

---
+++ android-online/app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.transsalomao.online"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.transsalomao.online"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
